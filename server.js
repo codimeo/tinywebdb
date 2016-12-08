c52239e26c46c4fd270493123f5c330d0e66e516
Nizar AYED <nizar.ayed@upgrade-code.org>
2016-12-08 17:51:20 +0100

    Working StoreValue and GetValue APIs. Should be checked on server.

--- server.js
+++ server.js
@@ -1,37 +1,60 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// Initilize datastore
+datastore = {};
 
 app.get('/', function (req, res) {
-  res.send('TinyWebDB by Nizar AYED');
+  var response = 'TinyWebDB by Nizar AYED<br>\n';
+  response += 'Datastore: <br>\n' + JSON.stringify( datastore );
+  res.send( response );
 })
 
 app.post('/storevalue', function (req, res) {
-  fs.readFile( __dirname + "/" + "tinydb.json", 'utf8', function (err, data) {
-    data = JSON.parse( data );
-
-    console.log( "Before : " );
-    console.log( JSON.stringify(data) );
-    data[ req.query.tag ] = req.query.value;
-    console.log( "After : " );
-    console.log( JSON.stringify( data ) );
-
-    fs.writeFile( "tinydb.json", JSON.stringify( data ), "utf8", (err) => {
-      if (err) throw err;
-      console.log('It\'s saved!');
-    });
-    res.send( JSON.stringify( data ) );
-  });
+  try {
+    var tag   = req.query.tag;
+    var value = req.query.value;
+    if( tag == null ) throw 'Tag is missing';
+    if( value == null ) value = '';
+    datastore[ tag ] = value;
+    console.log( "Tag: %s\nValue:%s\nDatastore : \n%s", tag, value, JSON.stringify( datastore ) );
+    res.json( datastore );
+  }
+  catch( err ) {
+    msg = {
+      error : err,
+      query : req.quey,
+      body : req.body
+    }
+    res.json( msg ); 
+  }
 })
 
 app.get('/getvalue', function (req, res) {
-  console.log(req.query.tag);
-  res.send(req.query.tag);
+  // Uncomment the following to debug
+  // console.log( "Tag : %s", req.query.tag );
+  // console.log( "DataStore : %s", JSON.stringify( datastore ) );
+  try {
+    if( req.query.tag == null || !req.query.tag ) throw 'Tag is missing.';
+    res.json( datastore[ req.query.tag ] );
+  } catch ( err ) {
+    msg = {
+      error : err,
+      query : req.quey,
+      body : req.body
+    };
+    res.json( msg ); 
+  }
 })
 
 var server = app.listen(8081, function () {
   var host = server.address().address
   var port = server.address().port
    
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("TinyWebDB app listening at http://%s:%s", host, port)
 })
